6a7d1e3313d530c4a551af5749a49e2b2fbd7d5d
remas <remikkrezel@gmail.com>
2018-03-21 03:39:15 +0100

    second commit

---
+++ resources/views/homepage/home.blade.php
@@ -0,0 +1,26 @@
+@extends('master')
+
+@section('style')
+<link rel="stylesheet" href="css/homepage/homepage.css">
+@endsection
+
+@section('content')
+
+<header class="homepage">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="logo">
+                <a href="#" class="font-homepage">Revelium</a>
+            </div>
+        </div>
+        <div class="row">
+            <p class="name">A place where your files will feel sheltered</p>
+        </div>
+        <div class="row">
+            <a href="#" class="btn btn-sm animated-button victoria-two">Store Into Revelium</a>
+        </div>
+    </div>
+
+</header>
+
+@endsection
