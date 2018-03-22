c0524652d5df5ce00c2332ba9a2d90c0db253697
remas <remikkrezel@gmail.com>
2018-03-22 15:25:17 +0100

    third commit

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -2,25 +2,40 @@
 
 @section('style')
 <link rel="stylesheet" href="css/homepage/homepage.css">
+<link rel="stylesheet" href="css/homepage/nav.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">
 @endsection
 
 @section('content')
 
+
+
 <header class="homepage">
     <div class="container-fluid">
-        <div class="row">
-            <div class="logo">
-                <a href="#" class="font-homepage">Revelium</a>
+        <div class="d-flex">
+            <div class="p-2">
+                <div class="logo">
+                    <a href="#" class="font-homepage">Relevium</a>
+                </div>
+            </div>
+            <div class="ml-auto p-2">
+                <i class="hamburger fas fa-bars fa-2x"></i>
             </div>
         </div>
+        {{--<div class="row">--}}
+            {{--<div class="logo">--}}
+                {{--<a href="#" class="font-homepage">Relevium</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
         <div class="row">
-            <p class="name">A place where your files will feel sheltered</p>
-        </div>
-        <div class="row">
-            <a href="#" class="btn btn-sm animated-button victoria-two">Store Into Revelium</a>
+            <div class="centered">
+                <h1 class="name">A place where your files will feel sheltered</h1>
+                <a href="#" class="btn btn-sm animated-button victoria-two">Store Into Revelium</a>
+            </div>
         </div>
     </div>
-
 </header>
+
 
 @endsection
